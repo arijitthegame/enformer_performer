1d88cfd645252287a3a0864c9c5f0e092d73e3d7
Arijit Sehanobish <arijitthegame@gmail.com>
2021-07-19 23:38:26 -0400

    Porting sonnet

--- performer/tf_version/fast_attention_sonnet.py
+++ performer/tf_version/fast_attention_sonnet.py
@@ -1,11 +1,174 @@
 import math
 import numpy as np
 import tensorflow as tf
-from util import *
 import sonnet as snt
-from fast_attention import create_projection_matrix, favor_attention,softmax_kernel_transformation
+from fast_attention import create_projection_matrix, favor_attention, softmax_kernel_transformation
 
 BIG_CONSTANT = 1e8
+
+_CHR_IDX = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m"]
+
+class DenseEinsum(snt.Module):
+  """A densely connected layer that uses tf.einsum as the backing computation.
+
+  This layer can perform einsum calculations of arbitrary dimensionality.
+
+  Arguments:
+    output_shape: Positive integer or tuple, dimensionality of the output space.
+    num_summed_dimensions: The number of dimensions to sum over. Standard 2D
+      matmul should use 1, 3D matmul should use 2, and so forth.
+    activation: Activation function to use. If you don't specify anything, no
+      activation is applied
+      (ie. "linear" activation: `a(x) = x`).
+    use_bias: Boolean, whether the layer uses a bias vector.
+    kernel_initializer: Initializer for the `kernel` weights matrix.
+    bias_initializer: Initializer for the bias vector.
+    kernel_regularizer: Regularizer function applied to the `kernel` weights
+      matrix.
+    bias_regularizer: Regularizer function applied to the bias vector.
+    activity_regularizer: Regularizer function applied to the output of the
+      layer (its "activation")..
+    kernel_constraint: Constraint function applied to the `kernel` weights
+      matrix.
+    bias_constraint: Constraint function applied to the bias vector.
+  Input shape:
+    N-D tensor with shape: `(batch_size, ..., input_dim)`. The most common
+      situation would be a 2D input with shape `(batch_size, input_dim)`.
+  Output shape:
+    N-D tensor with shape: `(batch_size, ..., units)`. For instance, for a 2D
+      input with shape `(batch_size, input_dim)`, the output would have shape
+      `(batch_size, units)`.
+  """
+
+  def __init__(self,
+               output_shape,
+               num_summed_dimensions=1,
+               activation=None,
+               use_bias=False,
+            #   kernel_initializer="glorot_uniform",
+            #   bias_initializer="zeros",
+               kernel_regularizer=None,
+               bias_regularizer=None,
+               activity_regularizer=None,
+               kernel_constraint=None,
+               bias_constraint=None,
+               **kwargs):
+    super(DenseEinsum, self).__init__(**kwargs)
+    self._output_shape = output_shape if isinstance(
+        output_shape, (list, tuple)) else (output_shape,)
+    self._activation = tf.keras.activations.get(activation)
+    self._use_bias = use_bias
+    self._kernel_initializer = snt.initializers.VarianceScaling(scale=1.0, mode='fan_avg', distribution='uniform')
+    self._bias_initializer = snt.initializers.Zeros
+    self._kernel_regularizer = tf.keras.regularizers.get(kernel_regularizer)
+    self._bias_regularizer = tf.keras.regularizers.get(bias_regularizer)
+    self._kernel_constraint = tf.keras.constraints.get(kernel_constraint)
+    self._bias_constraint = tf.keras.constraints.get(bias_constraint)
+    self._num_summed_dimensions = num_summed_dimensions
+   # self._einsum_string = None
+   
+
+  def _build_einsum_string(self, free_input_dims, bound_dims, output_dims):
+    input_str = ""
+    kernel_str = ""
+    output_str = ""
+    letter_offset = 0
+    for i in range(free_input_dims):
+      char = _CHR_IDX[i + letter_offset]
+      input_str += char
+      output_str += char
+
+    letter_offset += free_input_dims
+    for i in range(bound_dims):
+      char = _CHR_IDX[i + letter_offset]
+      input_str += char
+      kernel_str += char
+
+    letter_offset += bound_dims
+    for i in range(output_dims):
+      char = _CHR_IDX[i + letter_offset]
+      kernel_str += char
+      output_str += char
+
+    return input_str + "," + kernel_str + "->" + output_str
+
+  def build(self, input_shape):
+    input_shape = tf.TensorShape(input_shape)
+    input_rank = input_shape.rank
+    free_input_dims = input_rank - self._num_summed_dimensions
+    output_dims = len(self._output_shape)
+
+    self._einsum_string = self._build_einsum_string(free_input_dims,
+                                                    self._num_summed_dimensions,
+                                                    output_dims)
+
+    # This is only saved for testing purposes.
+    self._kernel_shape = (
+        input_shape[free_input_dims:].concatenate(self._output_shape))
+    print(self._kernel_shape)
+       
+    self._kernel = snt.initializers.VarianceScaling(scale=1.0, mode='fan_avg', distribution='uniform').__call__(self._kernel_shape, dtype=float)
+    print(self._kernel)
+          # "kernel", shape=self._kernel_shape,
+          # initializer=self._kernel_initializer,
+          # regularizer=self._kernel_regularizer,
+          # constraint=self._kernel_constraint,
+          # dtype=self.dtype,
+          # trainable=True)
+    
+    # if self._use_bias:
+    #   self._bias = self.add_weight(
+    #       "bias",
+    #       shape=self._output_shape,
+    #       initializer=self._bias_initializer,
+    #       regularizer=self._bias_regularizer,
+    #       constraint=self._bias_constraint,
+    #       dtype=self.dtype,
+    #       trainable=True)
+   # else:
+   # self._bias = None
+   # super(DenseEinsum, self).build(input_shape)
+
+  # def get_config(self):
+  #   config = {
+  #       "output_shape":
+  #           self._output_shape,
+  #       "num_summed_dimensions":
+  #           self._num_summed_dimensions,
+  #       "activation":
+  #           tf.keras.activations.serialize(self._activation),
+  #       "use_bias":
+  #           self._use_bias,
+  #       "kernel_initializer":
+  #           tf.keras.initializers.serialize(self._kernel_initializer),
+  #       "bias_initializer":
+  #           tf.keras.initializers.serialize(self._bias_initializer),
+  #       "kernel_regularizer":
+  #           tf.keras.regularizers.serialize(self._kernel_regularizer),
+  #       "bias_regularizer":
+  #           tf.keras.regularizers.serialize(self._bias_regularizer),
+  #       "activity_regularizer":
+  #           tf.keras.regularizers.serialize(self._activity_regularizer),
+  #       "kernel_constraint":
+  #           tf.keras.constraints.serialize(self._kernel_constraint),
+  #       "bias_constraint":
+  #           tf.keras.constraints.serialize(self._bias_constraint)
+  #   }
+  #   base_config = super(DenseEinsum, self).get_config()
+  #   return dict(list(base_config.items()) + list(config.items()))
+
+  def __call__(self, inputs):
+  
+    self._kernel = self.build(inputs.shape)
+    print(self.build(inputs.shape))
+    self._einsum_string = self._build_einsum_string(inputs.shape.rank - self._num_summed_dimensions, self._num_summed_dimensions, len(self._output_shape))
+    print(self._einsum_string)
+    ret = tf.einsum(self._einsum_string, inputs, self._kernel)
+    if self._use_bias:
+      ret += self._bias
+    if self._activation is not None:
+      ret = self._activation(ret)
+    return ret
 
 
 class Attention(snt.Module):
@@ -63,17 +226,17 @@ class Attention(snt.Module):
                                                 self.hidden_size)
     self.query_dense_layer = DenseEinsum(
         output_shape=(self.num_heads, size_per_head),
-        kernel_initializer=attention_initializer,
+      #  kernel_initializer=attention_initializer,
         use_bias=False,
         name="query")
     self.key_dense_layer = DenseEinsum(
         output_shape=(self.num_heads, size_per_head),
-        kernel_initializer=attention_initializer,
+     #   kernel_initializer=attention_initializer,
         use_bias=False,
         name="key")
     self.value_dense_layer = DenseEinsum(
         output_shape=(self.num_heads, size_per_head),
-        kernel_initializer=attention_initializer,
+      #  kernel_initializer=attention_initializer,
         use_bias=False,
         name="value")
 
@@ -81,10 +244,12 @@ class Attention(snt.Module):
     self.output_dense_layer = DenseEinsum(
         output_shape=self.hidden_size,
         num_summed_dimensions=2,
-        kernel_initializer=output_initializer,
+       # kernel_initializer=output_initializer,
         use_bias=False,
         name="output_transform")
-    super(Attention, self).build(input_shape)
+
+    return self.query_dense_layer, self.key_dense_layer, self.value_dense_layer, self.output_dense_layer
+  #  super(Attention, self).build(input_shape)
 
   def get_config(self):
     return {
@@ -124,6 +289,7 @@ class Attention(snt.Module):
     # Linearly project the query, key and value using different learned
     # projections. Splitting heads is automatically done during the linear
     # projections --> [batch_size, length, num_heads, dim_per_head].
+    self.query_dense_layer, self.key_dense_layer, self.value_dense_layer, self.output_dense_layer = self.build(query_input.shape)
     query = self.query_dense_layer(query_input)
     key = self.key_dense_layer(source_input)
     value = self.value_dense_layer(source_input)
